961e03c2c4faee1ec1f360bc51ed6b3833e753cc
morteify <michal.weglarz@protonmail.com>
2020-02-29 01:23:54 +0100

    Trim down views using generic class-based views

--- urbanpancake/api/views.py
+++ urbanpancake/api/views.py
@@ -1,36 +1,19 @@
-from rest_framework import mixins
 from rest_framework import generics
 from .models import User
 from .serializers import UserSerializer
 
 
-class UserList(mixins.ListModelMixin, mixins.CreateModelMixin, generics.GenericAPIView):
+class UserList(generics.ListCreateAPIView):
     """
     List all users, or crate a new user
     """
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
-    def get(self, request, *args, **kwargs):
-        return self.list(request, *args, **kwargs)
 
-    def post(self, request, *args, **kwargs):
-        return self.create(request, *args, **kwargs)
-
-
-class UserDetail(mixins.RetrieveModelMixin, mixins.UpdateModelMixin, mixins.DestroyModelMixin, generics.GenericAPIView):
+class UserDetail(generics.RetrieveUpdateDestroyAPIView):
     """
     Retrieve, update or delete a user
     """
-
-    queryset = User.obcjets.all()
+    queryset = User.objects.all()
     serializer_class = UserSerializer
-
-    def get(self, request, *args, **kwargs):
-        return self.retrieve(self, request, *args, **kwargs)
-
-    def put(self, request, *args, **kwargs):
-        return self.update(self, request, *args, **kwargs)
-
-    def delete(self, request, *args, **kwargs):
-        return self.destroy(self, request, *args, **kwargs)
